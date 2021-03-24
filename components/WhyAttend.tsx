d8bd0249e36a7da4a4c5dc87c824dd435b855066
Kam Figy <kam.figy@uniform.dev>
2021-03-24 12:13:03 -0700

    starter release

---
+++ components/WhyAttend.tsx
@@ -0,0 +1,25 @@
+import { PortableText, urlFor } from '../lib/sanityHelpers';
+import { WhyAttendDocument } from '../lib/sanityTypes';
+
+export const WhyAttend: React.FC<WhyAttendDocument> = ({ title, description, image }) => {
+  return (
+    <section className="bg-white border-b py-8">
+      <div className="container mx-auto flex flex-wrap pt-4 pb-12">
+        <div className="w-1/2">
+          <img src={urlFor(image).url()} className="p-10" />
+        </div>
+        <div className="w-1/2">
+          <div className="p-10">
+            <h2 className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800">
+              {title}
+            </h2>
+            <hr />
+            <div className="text-gray-800 p-10 whitespace-pre-line">
+              <PortableText blocks={description} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
